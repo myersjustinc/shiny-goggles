d082e7cadb33f9f67c892005eaad1a767d0ba9ec
Justin Myers <justin@justinmyers.net>
2016-03-19 10:38:44 -0400

    Start over in Reveal.js
    Let's stop worrying about how we'll code the interactions---this does
    what I want.
    
    Let's focus instead on the design and the content.

--- main.js
+++ main.js
@@ -1,50 +1,7 @@
-(function(window, framesConfig, $, _, Backbone) {
+(function(window, Reveal) {
 
-var app,
-  App,
-  appView,
-  AppView,
-  Frame,
-  FrameView;
-
-Frame = Backbone.Model.extend({});
-FrameView = Backbone.View.extend({
-  tagName: 'section',
-  className: 'frame',
-  render: function renderFrameView() {
-    this.$el.html(this.model.content);
-    return this;
-  }
+Reveal.initialize({
+  margin: 0
 });
 
-App = Backbone.Collection.extend({
-  model: Frame
-});
-AppView = Backbone.View.extend({
-  tagName: 'article',
-  className: 'app',
-  initialize: function initializeAppView(options) {
-    this.frameViews = this.collection.reduce(function(memo, frame) {
-      var frameView;
-
-      frameView = new FrameView(frame);
-      memo[frame.get('name')] = frameView;
-
-      return memo;
-    }, {});
-    console.log(this.frameViews);
-  },
-  render: function renderAppView() {
-    this.$el.html('');  // FIXME: Add this.
-    return this;
-  }
-});
-
-app = new App(framesConfig);
-appView = new AppView({
-  collection: app
-});
-
-appView.render().$el.appendTo($('body'));
-
-}(this, this.framesConfig, this.$, this._, this.Backbone));
+}(this, this.Reveal));
