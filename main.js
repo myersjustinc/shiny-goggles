ad8c16b34f359b7b75a80107461ea8c85b546eb4
Justin Myers <justin@justinmyers.net>
2016-03-19 13:05:34 -0400

    Properly define slide dimensions
    Just make everything 1080x1920 in all cases, since Reveal already is
    taking care of the scaling.

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 (function(window, Reveal) {
 
 // Shrink the video embeds as necessary to fit the viewport.
-(function(document) {
+function onYouTubeIframeAPIReady() {
   var windowAspectRatio,
     windowHeight = window.innerHeight,
     windowWidth = window.innerWidth,
-    youtubeVideos;
+    youtubeVideos,
+    YT = window.YT;
 
   windowAspectRatio = windowHeight / windowWidth;
 
-  youtubeVideos = document.querySelectorAll(
+  youtubeVideos = window.document.querySelectorAll(
     '.reveal .slides section iframe[src*="youtube"]');
+  console.log(youtubeVideos);
   Array.prototype.forEach.call(youtubeVideos, function(embed) {
     var embedAspectRatio,
       embedHeight = parseInt(embed.height, 10),
       embedWidth = parseInt(embed.width, 10),
       newHeight,
-      newWidth;
+      newWidth,
+      player;
 
     embedAspectRatio = embedHeight / embedWidth;
     if (embedAspectRatio <= windowAspectRatio) {
@@ -29,14 +32,42 @@
       newWidth = newHeight / embedAspectRatio;
     }
 
+    // Resize the frame to match.
     embed.height = newHeight;
     embed.width = newWidth;
+
+    // Construct a new YouTube player we can control.
+    player = new YT.Player(embed, {});
+    embed.videoPlayer = player;
   });
-}(window.document));
+}
 
 // Initialize the slideshow.
 Reveal.initialize({
-  margin: 0
+  height: 1920,
+  margin: 0,
+  width: 1080
+});
+
+// Reset all videos on slide change, and if the new current slide has any
+// videos on it, play them.
+Reveal.addEventListener('slidechanged', function checkVideo(event) {
+  var allVideos,
+    videosOnSlide;
+
+  allVideos = window.document.querySelectorAll(
+    '.reveal .slides section iframe[src*="youtube"]');
+  Array.prototype.forEach.call(allVideos, function(embed) {
+    var videoPlayer = embed.videoPlayer;
+    videoPlayer && videoPlayer.pauseVideo() && videoPlayer.seekTo(0);
+  });
+
+  videosOnSlide = event.currentSlide.querySelectorAll(
+    '.reveal .slides section iframe[src*="youtube"]');
+  Array.prototype.forEach.call(videosOnSlide, function(embed) {
+    var videoPlayer = embed.videoPlayer;
+    videoPlayer && videoPlayer.playVideo();
+  });
 });
 
 }(this, this.Reveal));
